f3b3f3fd62df892e72484b19f66177e4e081f29e
Ross Allan <ross@nallar.me>
2017-05-16 17:42:07 +0100

    Should now fail build if fails to extract DSL stats

--- root/root/install.sh
+++ root/root/install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+set -e
+
 apt-get update && apt-get install -y --force-yes --no-install-recommends \
     supervisor xinetd x11vnc xvfb tinywm openbox xdotool wmctrl x11-utils xterm libgtk2.0-0 wget curl \
     && apt-get autoclean && apt-get autoremove && rm -rf /var/lib/apt/lists/*
@@ -8,11 +10,10 @@ wget -O dslstats.tar.gz http://dslstats.me.uk/files/dslstats64L-5.9.tar.gz
 tar -zxvf dslstats.tar.gz
 rm dslstats.tar.gz
 mv dslstats64L-5.9 dslstats
-ls
 chmod +x dslstats/dslstats
 
 echo "Europe/London" > /etc/timezone
-rm /etc/localtime
+rm /etc/localtime || true
 dpkg-reconfigure -f noninteractive tzdata
 
 mkdir ~/.vnc
